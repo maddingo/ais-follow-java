8f624d17d598b26e508f6349296c3a18445af2b0
Martin Goldhahn <mgoldhahn@gmail.com>
2023-04-21 18:49:35 +0200

    initial checkin

---
+++ src/main/java/no/maddin/ais/AisFollowApplication.java
@@ -0,0 +1,13 @@
+package no.maddin.ais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisFollowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AisFollowApplication.class, args);
+    }
+
+}
